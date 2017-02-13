a2185dbffda08a826ff450c086d381022a8cd786
Unknown <ramon.vechini@gmail.com>
2017-02-12 23:34:21 -0200

    Dijkstra e Kruskal - Archive

---
+++ src/main/webapp/app/components/algorithms/agcmKruskal.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Ramon on 12/02/2017.
+ */
+(function () {
+    'use strict';
+
+    //algoritmo Árvore geradora de custo mínimo de Kruskal
+
+    angular.module('graphe.algorithms')
+        .service('agcmKruskal', function () {
+
+            //var n
+
+            //function run
+
+            //function cmDijkstra
+
+            //var service
+
+        }
---
+++ src/main/webapp/app/components/algorithms/cmDijkstra.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Ramon on 12/02/2017.
+ */
+(function () {
+    'use strict';
+
+    //algoritmo Caminhos Mínimos de Dijkstra
+
+    angular.module('graphe.algorithms')
+        .service('cmDijkstra', function () {
+
+            //var n
+
+            //function run
+
+            //function cmDijkstra
+            //1. cria min heap size = numero de vertices
+            //2. inicia com vértice escolhido de root = valor 0, todos os outros com infinito
+            //3. while min heap not empty do:
+            //  a. retira da pilha o adjacente ao root com menor distancia (chamado X)
+            //  b. checa adjacentes do X ainda contidos na pilha (chamados V)
+            //     se distancia de V for > que distancia de X + (X até V), update distancia
+
+            //var service
+
+        }
